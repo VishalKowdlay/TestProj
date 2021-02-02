6ac00ab8c3dbd34930cb3f0b7b875b95870db54f
VishalKowdlay <78388151+VishalKowdlay@users.noreply.github.com>
2021-02-01 19:13:57 -0500

    First Commit

--- app/src/main/java/com/example/githubproject/MainActivity.java
+++ app/src/main/java/com/example/githubproject/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.githubproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+Button button;
+TextView noClipLmao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noClipLmao = findViewById(R.id.clipped);
+        button = findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noClipLmao.setText("Clicked");
+            }
+        });
+
+
     }
 }
